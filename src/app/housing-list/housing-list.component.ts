f7c8b5a332800e011c45d29e4f156e06920b622c
PSewmuthu <apasindusewmuthu@gmail.com>
2023-10-14 15:32:33 +0530

    Imports Input and HousingLocation interface

--- src/app/housing-list/housing-list.component.ts
+++ src/app/housing-list/housing-list.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-housing-list',
   templateUrl: './housing-list.component.html',
-  styleUrls: ['./housing-list.component.css']
+  styleUrls: ['./housing-list.component.css'],
 })
 export class HousingListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
